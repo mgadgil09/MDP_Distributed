cc77248cfe23aabd2cf2d88304e083c09384d8c6
nitinsaroha <nitinsaroha@outlook.com>
2016-05-02 14:25:00 -0400

    removed classes

--- MDP_MPI/pymdptoolbox/src/mdptoolbox/final_mdp.py
+++ MDP_MPI/pymdptoolbox/src/mdptoolbox/final_mdp.py
@@ -42,9 +42,7 @@ import mdptoolbox.example as example
 from mpi4py import MPI
 
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+
 
 _MSG_STOP_MAX_ITER = "Iterating stopped due to maximum number of iterations " \
     "condition."
@@ -75,7 +73,7 @@ def _printVerbosity(iteration, variation):
     else:
         print("{:>10}{:>12}".format(iteration, variation))
 
-class final_mdp(object):
+class final_mdp():
     
     """A Markov Decision Problem.
 
@@ -92,7 +90,6 @@ class final_mdp(object):
         form is useful when the transition matrices are sparse as
         ``scipy.sparse.csr_matrix`` matrices can be used. In summary, each
         action's transition matrix must be indexable like ``transitions[a]``
-        where ``a`` ∈ {0, 1...A-1}, and ``transitions[a]`` returns an ``S`` ×
         ``S`` array-like object.
     reward : array
         Reward matrices or vectors. Like the transition matrices, these can
@@ -161,7 +158,7 @@ class final_mdp(object):
 
     """
 
-    def __init__(self, transitions, reward, discount, epsilon, max_iter,
+    def __init__(self, transitions, reward, discount, epsilon=0.01, max_iter=1000,
                  skip_check=False):
         # Initialise a MDP based on the input parameters.
 
@@ -229,7 +226,7 @@ class final_mdp(object):
         # If V hasn't been sent into the method, then we assume to be working
         # on the objects V attribute
         # print (a, b, ln)
-        print (rank)
+        # print (rank)
         if V is None:
             # this V should be a reference to the data rather than a copy
             V = self.V
@@ -395,282 +392,6 @@ class final_mdp(object):
         """Set the MDP algorithm to verbose mode."""
         self.verbose = True
 
-class ValueIteration(final_mdp):
-
-    """A discounted MDP solved using the value iteration algorithm.
-
-    Description
-    -----------
-    ValueIteration applies the value iteration algorithm to solve a
-    discounted MDP. The algorithm consists of solving Bellman's equation
-    iteratively.
-    Iteration is stopped when an epsilon-optimal policy is found or after a
-    specified number (``max_iter``) of iterations.
-    This function uses verbose and silent modes. In verbose mode, the function
-    displays the variation of ``V`` (the value function) for each iteration and
-    the condition which stopped the iteration: epsilon-policy found or maximum
-    number of iterations reached.
-
-    Parameters
-    ----------
-    transitions : array
-        Transition probability matrices. See the documentation for the ``MDP``
-        class for details.
-    reward : array
-        Reward matrices or vectors. See the documentation for the ``MDP`` class
-        for details.
-    discount : float
-        Discount factor. See the documentation for the ``MDP`` class for
-        details.
-    epsilon : float, optional
-        Stopping criterion. See the documentation for the ``MDP`` class for
-        details.  Default: 0.01.
-    max_iter : int, optional
-        Maximum number of iterations. If the value given is greater than a
-        computed bound, a warning informs that the computed bound will be used
-        instead. By default, if ``discount`` is not equal to 1, a bound for
-        ``max_iter`` is computed, otherwise ``max_iter`` = 1000. See the
-        documentation for the ``MDP`` class for further details.
-    initial_value : array, optional
-        The starting value function. Default: a vector of zeros.
-    skip_check : bool
-        By default we run a check on the ``transitions`` and ``rewards``
-        arguments to make sure they describe a valid MDP. You can set this
-        argument to True in order to skip this check.
-
-    Data Attributes
-    ---------------
-    V : tuple
-        The optimal value function.
-    policy : tuple
-        The optimal policy function. Each element is an integer corresponding
-        to an action which maximises the value function in that state.
-    iter : int
-        The number of iterations taken to complete the computation.
-    time : float
-        The amount of CPU time used to run the algorithm.
-
-    Methods
-    -------
-    run()
-        Do the algorithm iteration.
-    setSilent()
-        Sets the instance to silent mode.
-    setVerbose()
-        Sets the instance to verbose mode.
-
-    Notes
-    -----
-    In verbose mode, at each iteration, displays the variation of V
-    and the condition which stopped iterations: epsilon-optimum policy found
-    or maximum number of iterations reached.
-
-    Examples
-    --------
-    >>> import mdptoolbox, mdptoolbox.example
-    >>> P, R = mdptoolbox.example.forest()
-    >>> vi = mdptoolbox.mdp.ValueIteration(P, R, 0.96)
-    >>> vi.verbose
-    False
-    >>> vi.run()
-    >>> expected = (5.93215488, 9.38815488, 13.38815488)
-    >>> all(expected[k] - vi.V[k] < 1e-12 for k in range(len(expected)))
-    True
-    >>> vi.policy
-    (0, 0, 0)
-    >>> vi.iter
-    4
-
-    >>> import mdptoolbox
-    >>> import numpy as np
-    >>> P = np.array([[[0.5, 0.5],[0.8, 0.2]],[[0, 1],[0.1, 0.9]]])
-    >>> R = np.array([[5, 10], [-1, 2]])
-    >>> vi = mdptoolbox.mdp.ValueIteration(P, R, 0.9)
-    >>> vi.run()
-    >>> expected = (40.048625392716815, 33.65371175967546)
-    >>> all(expected[k] - vi.V[k] < 1e-12 for k in range(len(expected)))
-    True
-    >>> vi.policy
-    (1, 0)
-    >>> vi.iter
-    26
-
-    >>> import mdptoolbox
-    >>> import numpy as np
-    >>> from scipy.sparse import csr_matrix as sparse
-    >>> P = [None] * 2
-    >>> P[0] = sparse([[0.5, 0.5],[0.8, 0.2]])
-    >>> P[1] = sparse([[0, 1],[0.1, 0.9]])
-    >>> R = np.array([[5, 10], [-1, 2]])
-    >>> vi = mdptoolbox.mdp.ValueIteration(P, R, 0.9)
-    >>> vi.run()
-    >>> expected = (40.048625392716815, 33.65371175967546)
-    >>> all(expected[k] - vi.V[k] < 1e-12 for k in range(len(expected)))
-    True
-    >>> vi.policy
-    (1, 0)
-
-    """
-    def __init__(self, transitions, reward, discount, epsilon=0.01,
-                 max_iter=1000, initial_value=0, skip_check=False):
-        # Initialise a value iteration MDP.
-
-        final_mdp.__init__(self, transitions, reward, discount, epsilon, max_iter,
-                     skip_check=skip_check)
-
-        # initialization of optional arguments
-        if initial_value == 0:
-            self.V = _np.zeros(self.S)
-        else:
-            assert len(initial_value) == self.S, "The initial value must be " \
-                "a vector of length S."
-            self.V = _np.array(initial_value).reshape(self.S)
-        if self.discount < 1:
-            # compute a bound for the number of iterations and update the
-            # stored value of self.max_iter
-            self._boundIter(epsilon)
-            # computation of threshold of variation for V for an epsilon-
-            # optimal policy
-            self.thresh = epsilon * (1 - self.discount) / self.discount
-        else:  # discount == 1
-            # threshold of variation for V for an epsilon-optimal policy
-            self.thresh = epsilon
-
-    def _boundIter(self, epsilon):
-        # Compute a bound for the number of iterations.
-        #
-        # for the value iteration
-        # algorithm to find an epsilon-optimal policy with use of span for the
-        # stopping criterion
-        #
-        # Arguments -----------------------------------------------------------
-        # Let S = number of states, A = number of actions
-        #    epsilon   = |V - V*| < epsilon,  upper than 0,
-        #        optional (default : 0.01)
-        # Evaluation ----------------------------------------------------------
-        #    max_iter  = bound of the number of iterations for the value
-        #    iteration algorithm to find an epsilon-optimal policy with use of
-        #    span for the stopping criterion
-        #    cpu_time  = used CPU time
-        #
-        # See Markov Decision Processes, M. L. Puterman,
-        # Wiley-Interscience Publication, 1994
-        # p 202, Theorem 6.6.6
-        # k =    max     [1 - S min[ P(j|s,a), p(j|s',a')] ]
-        #     s,a,s',a'       j
-        k = 0
-        h = _np.zeros(self.S)
-
-        for ss in range(self.S):
-            PP = _np.zeros((self.A, self.S))
-            for aa in range(self.A):
-                try:
-                    PP[aa] = self.P[aa][:, ss]
-                except ValueError:
-                    PP[aa] = self.P[aa][:, ss].todense().A1
-            # minimum of the entire array.
-            h[ss] = PP.min()
-
-        k = 1 - h.sum()
-        Vprev = self.V
-        null, value = self._bellmanOperator()
-        # p 201, Proposition 6.6.5
-        span = getSpan(value - Vprev)
-        max_iter = (_math.log((epsilon * (1 - self.discount) / self.discount) /
-                    span) / _math.log(self.discount * k))
-        # self.V = Vprev
-
-        self.max_iter = int(_math.ceil(max_iter))
-    def get_range(self,rank, s):
-        local_n = s / size
-        local_a = rank * local_n
-        local_b = local_a + local_n
-        return (local_a, local_b)
-
-    def run(self):
-        # Run the value iteration algorithm.
-        self._startRun()
-        local_n = self.S / size
-        takeQ = _np.empty((local_n,self.A))
-        local_V = _np.zeros(self.S)
-        local_P = _np.zeros(self.S)
-        global_V =_np.zeros(self.S)
-        global_P =_np.zeros(self.S)
-        newrank = -1
-
-        while True:
-            self.iter += 1
-            local_a,local_b = self.get_range(rank,self.S)
-            Vprev = self.V.copy()
-
-            # Bellman Operator: compute policy and value functions
-            takeQ = self._bellmanOperator_mpi(local_a, local_b, local_n)
-            if rank == 0:
-                local_P = takeQ.argmax(axis=1)
-                local_V = takeQ.max(axis=1)
-                global_V[local_a:local_b] = local_V
-                global_P[local_a:local_b] = local_P
-                for i in xrange(1, size):
-                    status = MPI.Status()
-                    comm.Recv(takeQ, source = MPI.ANY_SOURCE, tag = MPI.ANY_TAG, status=status)
-                    newrank = status.Get_source()
-                    la,lb = self.get_range(newrank,self.S)
-                    local_P = takeQ.argmax(axis=1)
-                    local_V = takeQ.max(axis=1)
-                    global_V[la : lb] = local_V
-                    global_P[la : lb] = local_P
-                # print (repr(local_V) + "\n" + repr(local_P))
-                self.V = global_V
-                self.policy = global_P
-            else:
-                comm.send(takeQ, dest = 0, tag=rank)
-            comm.Bcast(self.V,root=0)
-            comm.Bcast(self.policy, root=0)
-
-            # The values, based on Q. For the function "max()": the option
-            # "axis" means the axis along which to operate. In this case it
-            # finds the maximum of the the rows. (Operates along the columns?)
-            variation = getSpan(self.V - Vprev)
-
-            if self.verbose:
-                _printVerbosity(self.iter, variation)
-
-            if variation < self.thresh:
-                if self.verbose:
-                    print(_MSG_STOP_EPSILON_OPTIMAL_POLICY)
-                break
-            elif self.iter == self.max_iter:
-                if self.verbose:
-                    print(_MSG_STOP_MAX_ITER)
-                break
-
-        self._endRun()
-
-"""Markov Decision Process (MDP) Toolbox: ``util`` module
-======================================================
-
-The ``util`` module provides functions to check that an MDP is validly
-described. There are also functions for working with MDPs while they are being
-solved.
-
-Available functions
--------------------
-
-:func:`~mdptoolbox.util.check`
-    Check that an MDP is properly defined
-:func:`~mdptoolbox.util.checkSquareStochastic`
-    Check that a matrix is square and stochastic
-:func:`~mdptoolbox.util.getSpan`
-    Calculate the span of an array
-:func:`~mdptoolbox.util.isNonNegative`
-    Check if a matrix has only non-negative elements
-:func:`~mdptoolbox.util.isSquare`
-    Check if a matrix is square
-:func:`~mdptoolbox.util.isStochastic`
-    Check if a matrix is row stochastic
-
-"""
-
 # import numpy as _np
 
 # import mdptoolbox.error as _error
@@ -679,7 +400,7 @@ _MDPERR = {
 "mat_nonneg" :
     "Transition probabilities must be non-negative.",
 "mat_square" :
-    "A transition probability matrix must be square, with dimensions S×S.",
+    "A",
 "mat_stoch" :
     "Each row of a transition probability matrix must sum to one (1).",
 "obj_shape" :
---
+++ MDP_MPI/pymdptoolbox/src/vi.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+import math as _math
+import time as _time
+import numpy as np
+import scipy.sparse as _sp
+import mdptoolbox.example as example
+from mpi4py import MPI
+import mdptoolbox
+from mdptoolbox.final_mdp import final_mdp
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+_MSG_STOP_MAX_ITER = "Iterating stopped due to maximum number of iterations " \
+    "condition."
+_MSG_STOP_EPSILON_OPTIMAL_POLICY = "Iterating stopped, epsilon-optimal " \
+    "policy found."
+_MSG_STOP_EPSILON_OPTIMAL_VALUE = "Iterating stopped, epsilon-optimal value " \
+    "function found."
+_MSG_STOP_UNCHANGING_POLICY = "Iterating stopped, unchanging policy found."
+
+def _boundIter(mdp_obj):
+
+    k = 0
+    h = np.zeros(mdp_obj.S)
+
+    for ss in range(mdp_obj.S):
+        PP = np.zeros((mdp_obj.A, mdp_obj.S))
+        for aa in range(mdp_obj.A):
+            try:
+                PP[aa] = mdp_obj.P[aa][:, ss]
+            except ValueError:
+                PP[aa] = mdp_obj.P[aa][:, ss].todense().A1
+        # minimum of the entire array.
+        h[ss] = PP.min()
+
+    k = 1 - h.sum()
+    Vprev = mdp_obj.V
+    null, value = mdp_obj._bellmanOperator()
+    # p 201, Proposition 6.6.5
+    span = getSpan(value - Vprev)
+    max_iter = (_math.log((mdp_obj.epsilon * (1 - mdp_obj.discount) / mdp_obj.discount) /
+                span) / _math.log(mdp_obj.discount * k))
+    # mdp_obj.V = Vprev
+
+    mdp_obj.max_iter = int(_math.ceil(max_iter))
+def get_range(rank, s):
+    local_n = s / size
+    local_a = rank * local_n
+    local_b = local_a + local_n
+    return (local_a, local_b)
+
+def run(mdp_obj):
+    # Run the value iteration algorithm.
+    mdp_obj._startRun()
+    local_n = mdp_obj.S / size
+    takeQ = np.empty((local_n,mdp_obj.A))
+    local_V = np.zeros(mdp_obj.S)
+    local_P = np.zeros(mdp_obj.S)
+    global_V =np.zeros(mdp_obj.S)
+    global_P =np.zeros(mdp_obj.S)
+    newrank = -1
+
+    while True:
+        mdp_obj.iter += 1
+        local_a,local_b = get_range(rank,mdp_obj.S)
+        Vprev = mdp_obj.V.copy()
+
+        # Bellman Operator: compute policy and value functions
+        takeQ = mdp_obj._bellmanOperator_mpi(local_a, local_b, local_n)
+        if rank == 0:
+            local_P = takeQ.argmax(axis=1)
+            local_V = takeQ.max(axis=1)
+            global_V[local_a:local_b] = local_V
+            global_P[local_a:local_b] = local_P
+            for i in xrange(1, size):
+                status = MPI.Status()
+                comm.Recv(takeQ, source = MPI.ANY_SOURCE, tag = MPI.ANY_TAG, status=status)
+                newrank = status.Get_source()
+                la,lb = mdp_obj.get_range(newrank,mdp_obj.S)
+                local_P = takeQ.argmax(axis=1)
+                local_V = takeQ.max(axis=1)
+                global_V[la : lb] = local_V
+                global_P[la : lb] = local_P
+            # print (repr(local_V) + "\n" + repr(local_P))
+            mdp_obj.V = global_V
+            mdp_obj.policy = global_P
+        else:
+            comm.send(takeQ, dest = 0, tag=rank)
+        comm.Bcast(mdp_obj.V, root=0)
+        comm.Bcast(mdp_obj.policy, root=0)
+
+        # The values, based on Q. For the function "max()": the option
+        # "axis" means the axis along which to operate. In this case it
+        # finds the maximum of the the rows. (Operates along the columns?)
+        variation = getSpan(mdp_obj.V - Vprev)
+
+        if mdp_obj.verbose:
+            _printVerbosity(mdp_obj.iter, variation)
+
+        if variation < mdp_obj.thresh:
+            if mdp_obj.verbose:
+                print(_MSG_STOP_EPSILON_OPTIMAL_POLICY)
+            break
+        elif mdp_obj.iter == mdp_obj.max_iter:
+            if mdp_obj.verbose:
+                print(_MSG_STOP_MAX_ITER)
+            break
+
+    mdp_obj._endRun()
+    print(mdp_obj.V)
+    print(mdp_obj.policy)
+
+def _printVerbosity(iteration, variation):
+    if isinstance(variation, float):
+        print("{:>10}{:>12f}".format(iteration, variation))
+    elif isinstance(variation, int):
+        print("{:>10}{:>12d}".format(iteration, variation))
+    else:
+        print("{:>10}{:>12}".format(iteration, variation))
+
+def getSpan(array):
+    """Return the span of `array`
+
+    span(array) = max array(s) - min array(s)
+
+    """
+    return array.max() - array.min()
+
+P, R = mdptoolbox.example.forest(S=4)
+mdp_obj = final_mdp(P,R,0.96)
+
+initial_value=0
+if initial_value == 0:
+    mdp_obj.V = np.zeros(mdp_obj.S)
+else:
+    assert len(initial_value) == mdp_obj.S, "The initial value must be " \
+        "a vector of length S."
+    mdp_obj.V = np.array(initial_value).reshape(mdp_obj.S)
+if mdp_obj.discount < 1:
+    # compute a bound for the number of iterations and update the
+    # stored value of mdp_obj.max_iter
+    _boundIter(mdp_obj)
+    # computation of threshold of variation for V for an epsilon-
+    # optimal policy
+    mdp_obj.thresh = mdp_obj.epsilon * (1 - mdp_obj.discount) / mdp_obj.discount
+else:  # discount == 1
+    # threshold of variation for V for an epsilon-optimal policy
+    mdp_obj.thresh = mdp_obj.epsilon
+
+run(mdp_obj)
